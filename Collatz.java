25d76514f3f441aba5052d80e2c242d0fed803de
RonniHorn <ronni.horn@post.runi.ac.il>
2025-11-07 22:38:28 +0200

    Collatz

--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,36 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
+		int seed = Integer.parseInt(args[0]);
+		String mode = args[1];
+		int actions = 0;
+		int currentSeed = seed; // the seed we are making actions on
+		if(mode.equals("v"))
+		{
+			for(int i = 1; i<=seed; i++)
+			{
+				currentSeed = i;
+				System.out.print(currentSeed + " ");
+				actions = 1;
+				while (currentSeed != 1){
+					actions++;
+					if(currentSeed%2 == 0){
+						currentSeed = currentSeed/2;
+					}
+					else
+					{
+						currentSeed = (currentSeed * 3) + 1;
+					}
+					System.out.print(currentSeed + " ");
+				}
+				System.out.println("(" + actions + ")");
+					
+			}
+		}
+		else
+		{
+			System.out.println("Every one of the first " + seed + " hailstone sequences reached 1.");
+		}
+
 	}
 }
