badec5658d16c5496d5194a98e27677f90dd7af4
RonniHorn <ronni.horn@post.runi.ac.il>
2025-11-07 22:52:39 +0200

    Cheers

--- Cheers.java
+++ Cheers.java
@@ -8,6 +8,28 @@ Great work boaz!
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String name = args[0];
+            int num = Integer.parseInt(args[1]);
+            int length = name.length();
+            String an = "AEFHILMSXNOR";
+           for(int i = 0; i<length; i++){
+                int j = 0;
+                boolean didPrint = false;
+                while(j<an.length() && didPrint == false){
+
+                        if(name.charAt(i) == an.charAt(j)){
+                                System.out.println("Give me an " + name.charAt(i) + ": " + name.charAt(i) + "!" );
+                                didPrint = true;
+                        }
+                        j++;
+                }
+                if(j == an.length() && didPrint == false)
+                        System.out.println("Give me a " + name.charAt(i) + ": " + name.charAt(i) + "!" );
+
+            }
+        System.out.println("What does that spell?");
+        for(int i = 0; i<num; i++){
+             System.out.println(name + "!!!");
+        }
         }
 }
